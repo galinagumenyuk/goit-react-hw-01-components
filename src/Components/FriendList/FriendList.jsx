c8f16dc5458bfbb1d9747c5a27cfb2537c3da59a
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-08 12:38:43 +0200

    remove div

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -4,18 +4,16 @@ import s from "./FriendList.module.css";
 import Item from "./Item";
 
 const FriendList = ({ friends }) => (
-  <div>
-    <ul className={s.friendList}>
-      {friends.map((friend) => (
-        <Item
-          key={friend.id}
-          name={friend.name}
-          avatar={friend.avatar}
-          isOnline={friend.isOnline}
-        />
-      ))}
-    </ul>
-  </div>
+  <ul className={s.friendList}>
+    {friends.map((friend) => (
+      <Item
+        key={friend.id}
+        name={friend.name}
+        avatar={friend.avatar}
+        isOnline={friend.isOnline}
+      />
+    ))}
+  </ul>
 );
 
 FriendList.propTypes = {
--- src/Components/TransactionHistory/TransactionHistory.jsx
+++ src/Components/TransactionHistory/TransactionHistory.jsx
@@ -4,27 +4,25 @@ import s from "./TransactionHistory.module.css";
 import Transaction from "./Transaction";
 
 const TransactionHistory = ({ items }) => (
-  <div>
-    <table className={s.transactionHistory}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => (
-          <Transaction
-            key={item.id}
-            type={item.type}
-            amount={item.amount}
-            currency={item.currency}
-          />
-        ))}
-      </tbody>
-    </table>
-  </div>
+  <table className={s.transactionHistory}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map((item) => (
+        <Transaction
+          key={item.id}
+          type={item.type}
+          amount={item.amount}
+          currency={item.currency}
+        />
+      ))}
+    </tbody>
+  </table>
 );
 
 TransactionHistory.propTypes = {
